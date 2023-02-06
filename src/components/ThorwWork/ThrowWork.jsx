2b7068ed51719d36715e7ed84e2bea527ace5cd8
NewTreetion2 <qkrgusdnd8023@gmail.com>
2023-02-06 23:29:31 +0900

    0206 일 던져주기 img 파트 추가

--- src/components/ThorwWork/ThrowWork.jsx
+++ src/components/ThorwWork/ThrowWork.jsx
@@ -1,15 +1,40 @@
+import { useRef } from "react";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
+import "./ThrowWork.css";
 
 export default function ThrowWork() {
+  const imgRef = useRef();
+  const imgChange = (e) => {
+    const selectedFile = e.target.files;
+    imgRef.current.src = `img/${selectedFile[0].name}`;
+    imgRef.current.style = { objectFit: "contain" };
+  };
+
   return (
     <Form>
       <Row className="mb-3">
         <Form.Group controlId="formFile" className="mb-3">
-          <Form.Label>이미지를 선택해주세요</Form.Label>
-          <Form.Control type="file" size="sm" />
+          <Form.Label>
+            <div className="imgPreviewContainer">
+              <img
+                className="imgPreview"
+                src="img/imgUpload.PNG"
+                alt="이미지 없음"
+                ref={imgRef}
+              />
+            </div>
+            <p>이미지를 선택해주세요</p>
+          </Form.Label>
+          <Form.Control
+            type="file"
+            size="sm"
+            accept="image/*"
+            style={{ display: "none" }}
+            onChange={imgChange}
+          />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridEmail">
