3eb9214568140d0ec569ebaf1b3e6afebf6310e0
NewTreetion2 <115389778+NewTreetion2@users.noreply.github.com>
2023-03-29 22:27:55 +0900

    0329 search 일부추가, 마이페이지 보강

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -2,17 +2,31 @@ import styles from "./Summary.module.scss";
 import SummaryBox from "components/SummaryBox/SummaryBox";
 
 export default function Summary() {
+  // func에는 현재 Worklist의 정보를 가져와 count를 보여주는 기능 1
+  // onClick시 던져준 작업, 받은 작업, 등록순 등으로 sort해주는 기능 2
   return (
     <>
       <div className={`${styles.summaryFunctionBox}`}>
         <div className={`${styles.summaryContent}`}>
-          <SummaryBox img="1" text="진행중" func="3" />
+          <SummaryBox
+            img={<img src="img/rect1.png" alt="rect1"></img>}
+            text="던져준 일"
+            func="3"
+          />
         </div>
         <div className={`${styles.summaryContent}`}>
-          <SummaryBox img="1" text="작업 발송중" func="3" />
+          <SummaryBox
+            img={<img src="img/rect2.png" alt="rect2"></img>}
+            text="받은 일"
+            func="3"
+          />
         </div>
         <div className={`${styles.summaryContent}`}>
-          <SummaryBox img="1" text="취소·문제 해결" func="3" />
+          <SummaryBox
+            img={<img src="img/rect3.png" alt="rect3"></img>}
+            text="등록된 프로젝트"
+            func="3"
+          />
         </div>
         <div className={`${styles.summaryContent}`}>4</div>
       </div>
--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,26 +1,24 @@
-import MyButton from "components/Button/MyButton";
-import Menu from "components/Menu/Menu";
-import Summary from "components/Summary/Summary";
 import styles from "./MyPage.module.scss";
 
+import Menu from "components/Menu/Menu";
+import Profile from "components/Profile/Profile";
+import Summary from "components/Summary/Summary";
+
+import { Form } from "react-bootstrap";
+import { useRecoilState } from "recoil";
+import { projectSearchStatus } from "store";
+
 export default function MyPage() {
+  const [searchState, setSearchState] = useRecoilState(projectSearchStatus);
+  function onChangeHandler(e) {
+    setSearchState(e.target.value);
+  }
   return (
     <div className={`${styles.main}`}>
       <div className={`${styles.infoMenuBox}`}>
         <div className={`${styles.userInfo}`}>
-          <div className={`${styles.profile}`}>
-            <img
-              className={`${styles.profileImg}`}
-              src="img/default_profile.png"
-              alt="프로필 디폴트 이미지"
-            />
-          </div>
-          <div className={`${styles.userName}`}>트리션</div>
-          <div className={`${styles.profileImgChangeBtn}`}>
-            <MyButton text="프로필 사진 변경" />
-          </div>
+          <Profile />
         </div>
-        {/* 들어갈 내용 -> 유저 아이디와 대표 이미지 */}
         <div className={`${styles.menuBar}`}>
           <Menu />
         </div>
@@ -30,8 +28,16 @@ export default function MyPage() {
           <h5>판매관리</h5>
           <Summary />
         </div>
-        <div className={`${styles.search}`}>검색</div>
-        {/* 검색 기능 -> 프로젝트를 이름순, 등록순, 내가 신청한 프로젝트, 내가 공고한 프로젝트 등 */}
+        <div className={`${styles.search}`}>
+          <Form.Select
+            className={`${styles.select}`}
+            onChange={onChangeHandler}
+          >
+            <option value="request">던져준 일</option>
+            <option value="proceed">받은 일</option>
+            <option value="all_project">등록된 프로젝트</option>
+          </Form.Select>
+        </div>
         <div className={`${styles.workList}`}>워크 리스트</div>
         {/* 현재 내가 참여하고 있는 프로젝트를 등록순으로 정리, 이후 검색에 따라 나열순서가 변경된다*/}
       </div>
