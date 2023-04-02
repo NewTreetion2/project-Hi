a2dbfe481bb42f571ea66012959e8d05c4cabe85
NewTreetion2 <115389778+NewTreetion2@users.noreply.github.com>
2023-04-02 18:22:36 +0900

    0402 일 받기 페이지 완성

---
+++ src/components/WorkListCompo/WorkListFilter/WorkListFilter.jsx
@@ -0,0 +1,62 @@
+import styles from "./WorkListFilter.module.scss";
+import "react-datepicker/dist/react-datepicker.css";
+
+//! TODO: 왜 컴포넌트를 그대로?? , 다른것들은 컴포넌트의 index에서 가져옴
+import MySelect from "components/MySelect/MySelect";
+import MyButton from "components/Button/MyButton";
+
+import { useWorkListFilter } from "hooks";
+
+const categoryArr = [
+  { value: "all", text: "전체" },
+  { value: "animation", text: "애니메이션" },
+  { value: "advert", text: "광고" },
+  { value: "game", text: "게임" },
+];
+
+const priceArr = [
+  { value: "all", text: "전체" },
+  { value: "0", text: "500,000원 이상" },
+  { value: "50", text: "500,000원 미만" },
+  { value: "20", text: "200,000원 미만" },
+  { value: "10", text: "100,000원 미만" },
+  { value: "5", text: "50,000원 미만" },
+  { value: "3", text: "30,000원 미만" },
+];
+
+export default function Filter() {
+  const {
+    categoryFilter,
+    priceFilter,
+    handleCategoryChange,
+    handlePriceChange,
+  } = useWorkListFilter();
+
+  console.log(categoryFilter, priceFilter);
+
+  return (
+    <div className={styles.selectorcontainer}>
+      <div className={styles.selectorWrapper}>
+        <p className={styles.selectExplaneText}>카테고리</p>
+        <MySelect
+          id="project"
+          onChangeHandler={handleCategoryChange}
+          arr={categoryArr}
+        />
+      </div>
+
+      <div className={styles.selectorWrapper}>
+        <p className={styles.selectExplaneText}>금액</p>
+        <MySelect
+          id="project"
+          onChangeHandler={handlePriceChange}
+          arr={priceArr}
+        />
+      </div>
+
+      <div className={`${styles.commitBtn}`}>
+        <MyButton onClickHandler={() => {}} text="조회" />
+      </div>
+    </div>
+  );
+}
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useInput } from "hooks/useInput";
 export { default as useModalControl } from "hooks/useModalControl";
+export { default as useWorkListFilter } from "hooks/useWorkListFilter";
